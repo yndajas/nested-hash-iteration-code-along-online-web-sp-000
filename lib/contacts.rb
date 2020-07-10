6e2ca0196eeca6af19a98a192fb858feedad1c12
Ynda Jas <yndajas@gmail.com>
2020-07-10 02:11:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       attributes_hash.each do |attribute, value|
         if attribute == :favorite_ice_cream_flavors
-          value.delete_if{|ice_cream|} ice_cream == "strawberry"
+          value.delete_if{|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
